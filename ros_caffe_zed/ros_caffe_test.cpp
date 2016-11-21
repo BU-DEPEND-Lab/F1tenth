ffe5f6ccae213d240cbd9d3839ecfc3fdde0ccd6
bu <hcguo2015@gmail.com>
2016-11-21 15:36:39 -0500

    add using zed with caffe code

---
+++ ros_caffe_zed/ros_caffe_test.cpp
@@ -0,0 +1,94 @@
+/*_caffe_test.cpp
+ *
+ *  Created on: Aug 31, 2015
+ *      Author: Tzutalin
+ */
+
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <std_msgs/String.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include "Classifier.h"
+#include "time.h"
+const std::string RECEIVE_IMG_TOPIC_NAME = "/camera/rgb/image_rect_color";
+const std::string PUBLISH_RET_TOPIC_NAME = "/caffe_ret";
+
+Classifier* classifier;
+std::string model_path;
+std::string weights_path;
+std::string mean_file;
+std::string label_file;
+std::string image_path;
+
+ros::Publisher gPublisher;
+
+void publishRet(const std::vector<Prediction>& predictions);
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+    try {
+        cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
+        //cv::imwrite("rgb.png", cv_ptr->image);
+		cv::Mat img = cv_ptr->image;
+	float begin = clock();	
+std::vector<Prediction> predictions = classifier->Classify(img);
+        float end = clock();
+        float dur = (end - begin) / CLOCKS_PER_SEC ;        
+    for (size_t i = 0; i < predictions.size(); ++i) {
+        Prediction p = predictions[i];
+        std::cout << std::fixed << std::setprecision(4) << p.second << " - \"" << p.first << "\"" << dur << "second"<<std::endl;
+    }
+	    publishRet(predictions);
+    } catch (cv_bridge::Exception& e) {
+        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+    }
+}
+
+// TODO: Define a msg or create a service
+// Try to receive : $rostopic echo /caffe_ret
+void publishRet(const std::vector<Prediction>& predictions)  {
+    std_msgs::String msg;
+    std::stringstream ss;
+    for (size_t i = 0; i < predictions.size(); ++i) {
+        Prediction p = predictions[i];
+        ss << "[" << p.second << " - " << p.first << "]" << std::endl;
+    }
+    msg.data = ss.str();
+    gPublisher.publish(msg);
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "ros_caffe_test");
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it_zed(nh);
+    // To receive an image from the topic, PUBLISH_RET_TOPIC_NAME
+    const std::string ROOT_SAMPLE = ros::package::getPath("ros_caffe");
+    model_path = ROOT_SAMPLE + "/data/deploy.prototxt";
+    weights_path = ROOT_SAMPLE + "/data/bvlc_reference_caffenet.caffemodel";
+    mean_file = ROOT_SAMPLE + "/data/imagenet_mean.binaryproto";
+    label_file = ROOT_SAMPLE + "/data/synset_words.txt";
+    image_path = ROOT_SAMPLE + "/data/cat.jpg";
+    float time1 = clock();
+    classifier = new Classifier(model_path, weights_path, mean_file, label_file);
+    float time2 = clock();
+    float dur = time2 - time1;
+    std::cout<< dur << std::endl;
+    image_transport::Subscriber sub = it_zed.subscribe(RECEIVE_IMG_TOPIC_NAME, 1, imageCallback);
+        gPublisher = nh.advertise<std_msgs::String>(PUBLISH_RET_TOPIC_NAME, 100);
+
+    // Test data/cat.jpg
+    cv::Mat img = cv::imread(image_path, -1);
+    std::vector<Prediction> predictions = classifier->Classify(img);
+    /* Print the top N predictions. */
+    std::cout << "Test default image under /data/cat.jpg" << std::endl;
+    for (size_t i = 0; i < predictions.size(); ++i) {
+        Prediction p = predictions[i];
+        std::cout << std::fixed << std::setprecision(4) << p.second << " - \"" << p.first << "\"" << std::endl;
+    }
+	publishRet(predictions);
+
+    ros::spin();
+    delete classifier;
+    ros::shutdown();
+    return 0;
+}
